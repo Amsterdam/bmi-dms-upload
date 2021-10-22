b531e0744b7eb2281db624c2004cd665490fdddf
vdagraca <vivian.da.graca@gmail.com>
2021-10-22 22:07:18 +0200

    feat(step1): style step 1

--- src/components/Wizard/Step1.tsx
+++ src/components/Wizard/Step1.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FileUpload, FileUploadProps, CustomFile } from '@amsterdam/bmi-component-library';
 import { CustomFileOrRejection } from '@amsterdam/bmi-component-library/lib/common/src/FileUpload/hooks';
+import { Step1Styles } from './Step1Styles';
 
 type Props = {
 	getPostUrl: FileUploadProps['getPostUrl'];
@@ -12,21 +13,23 @@ type Props = {
 
 const Step1: React.FC<Props> = ({ getPostUrl, getHeaders, onFileRemove, onFileSuccess, storedFiles }) => {
 	return (
-		<FileUpload
-			getPostUrl={getPostUrl}
-			placeholder="Sleep de png bestanden in dit vlak of"
-			droppingLabel="bestanden geselecteerd"
-			selectFilesLabel="selecteer bestanden"
-			removeLabel="Wissen"
-			cancelLabel="Annuleren"
-			fileUploadErrorLabel="dit bestand kan niet worden geüpload"
-			fileUploadInProgressLabel="wordt geupload"
-			options={{ noClick: true, noKeyboard: true }}
-			onFileRemove={onFileRemove}
-			onFileSuccess={onFileSuccess}
-			getHeaders={getHeaders}
-			storedFiles={storedFiles as CustomFileOrRejection[]}
-		/>
+		<Step1Styles>
+			<FileUpload
+				getPostUrl={getPostUrl}
+				placeholder="Sleep de png bestanden in dit vlak of"
+				droppingLabel="bestanden geselecteerd"
+				selectFilesLabel="selecteer bestanden"
+				removeLabel="Wissen"
+				cancelLabel="Annuleren"
+				fileUploadErrorLabel="dit bestand kan niet worden geüpload"
+				fileUploadInProgressLabel="wordt geupload"
+				options={{ noClick: true, noKeyboard: true }}
+				onFileRemove={onFileRemove}
+				onFileSuccess={onFileSuccess}
+				getHeaders={getHeaders}
+				storedFiles={storedFiles as CustomFileOrRejection[]}
+			/>
+		</Step1Styles>
 	);
 };
 
