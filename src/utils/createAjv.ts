f1d2d0953793fc506de775d71d60b4f61d4eb10c
Fortunato Geelhoed <f.geelhoed@gmail.com>
2021-11-16 16:25:34 +0100

    refactor(createajv): better method of creating/exporting a re-usable ajv instance

--- src/utils/createAjv.ts
+++ src/utils/createAjv.ts
@@ -2,29 +2,23 @@ import AJV from 'ajv';
 import addFormats from 'ajv-formats';
 import ajvErrors from 'ajv-errors';
 
-let cached: AJV;
+const ajv = new AJV({
+	allErrors: true,
+	verbose: true,
+	strict: false,
+});
 
-export default function () {
-	if (cached) return cached;
+addFormats(ajv);
 
-	const ajv = new AJV({
-		allErrors: true,
-		verbose: true,
-		strict: false,
-	});
+ajv.addKeyword({
+	keyword: 'bmi-isNotEmpty',
+	type: 'string',
+	validate: function (schema: any, data: any) {
+		return typeof data === 'string' && data.trim() !== '';
+	},
+	errors: true,
+});
 
-	addFormats(ajv);
+ajvErrors(ajv, { singleError: true, keepErrors: false });
 
-	ajv.addKeyword({
-		keyword: 'bmi-isNotEmpty',
-		type: 'string',
-		validate: function (schema: any, data: any) {
-			return typeof data === 'string' && data.trim() !== '';
-		},
-		errors: true,
-	});
-
-	cached = ajvErrors(ajv, { singleError: true, keepErrors: false });
-
-	return cached;
-}
+export default ajv;
