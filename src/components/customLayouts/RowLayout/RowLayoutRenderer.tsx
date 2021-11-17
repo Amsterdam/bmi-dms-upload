73bd2b98dbeaea9d3ea8901e1381c737bbb8a686
Fortunato Geelhoed <f.geelhoed@gmail.com>
2021-11-17 09:04:10 +0100

    refactor(rowlayoutrenderer): got rid of explicit return statement

--- src/components/customLayouts/RowLayout/RowLayoutRenderer.tsx
+++ src/components/customLayouts/RowLayout/RowLayoutRenderer.tsx
@@ -3,26 +3,24 @@ import { ResolvedJsonFormsDispatch, withJsonFormsLayoutProps } from '@jsonforms/
 import { LayoutProps, Layout } from '@jsonforms/core';
 import RowStyles from './RowStyles';
 
-const RowLayoutRenderer = ({ schema, uischema, path, renderers, cells, enabled, visible }: LayoutProps) => {
-	return (
-		<div>
-			{visible &&
-				(uischema as Layout).elements.map((child, idx) => {
-					return (
-						<RowStyles key={`row-${idx}`}>
-							<ResolvedJsonFormsDispatch
-								schema={schema}
-								uischema={child}
-								path={path}
-								enabled={enabled}
-								renderers={renderers}
-								cells={cells}
-							/>
-						</RowStyles>
-					);
-				})}
-		</div>
-	);
-};
+const RowLayoutRenderer = ({ schema, uischema, path, renderers, cells, enabled, visible }: LayoutProps) => (
+	<div>
+		{visible &&
+			(uischema as Layout).elements.map((child, idx) => {
+				return (
+					<RowStyles key={`row-${idx}`}>
+						<ResolvedJsonFormsDispatch
+							schema={schema}
+							uischema={child}
+							path={path}
+							enabled={enabled}
+							renderers={renderers}
+							cells={cells}
+						/>
+					</RowStyles>
+				);
+			})}
+	</div>
+);
 
 export default withJsonFormsLayoutProps(RowLayoutRenderer);
