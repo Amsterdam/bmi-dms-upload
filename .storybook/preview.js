6c62c76b421b3240a5be5359c0533dcb4ccfffc7
Fortunato Geelhoed <f.geelhoed@gmail.com>
2021-11-17 08:19:10 +0100

    refactor(storybook preview): reverting to default export for react

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { GlobalStyle, ThemeProvider } from '@amsterdam/asc-ui';
 import { ThemeProvider as MUIThemeProvider } from '@material-ui/core/styles';
 import muiTheme from '../src/theme/material-ui-theme';
