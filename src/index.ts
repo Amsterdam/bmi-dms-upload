09d38ed9fcaaed4743db2e3b9b84989fb1e76286
Fortunato Geelhoed <f.geelhoed@gmail.com>
2021-11-22 11:05:43 +0100

    feat(types): exporting json schema types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export {
 	default as AddDocumentButton,
 	Props as AddDocumentProps,
 } from './components/AddDocumentButton/AddDocumentButton';
+
+export { RowLayoutSchema, CustomJsonSchema } from './types';
