93bb0223e19c06e26cc8e47b4e42c77ae89a1110
Johan Hage <johan@sugarmade.nl>
2021-12-02 09:46:44 +0100

    feat(bulkmetadata): updated by review comments

--- src/components/StaticMetadata/StaticMetadata.tsx
+++ src/components/StaticMetadata/StaticMetadata.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { ComponentProps } from 'react';
 import { DescriptionList } from '@amsterdam/bmi-component-library';
 import { DescriptionListStyle } from './StaticMetadataStyles';
 
 export type Props = {
-	list: DescriptionList;
+	list: ComponentProps<typeof DescriptionList>['list'];
 };
 
 const StaticMetadata: React.FC<Props> = ({ list }) => <DescriptionListStyle list={list} />;
