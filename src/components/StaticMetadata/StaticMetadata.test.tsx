7112ed824461a3b0e0d3af9f2079401efea434a8
Johan Hage <johan@sugarmade.nl>
2021-12-03 11:54:31 +0100

    feat(bulkmetadata): updated by review comments

--- src/components/StaticMetadata/StaticMetadata.test.tsx
+++ src/components/StaticMetadata/StaticMetadata.test.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import * as cl from '@amsterdam/bmi-component-library';
 import renderWithTheme from '../../../tests/utils/withTheme';
 import StaticMetadata from './StaticMetadata';
 import { metadataList } from './__stubs__';
 
+jest.mock('@amsterdam/bmi-component-library', () => ({
+	...jest.requireActual('@amsterdam/bmi-component-library'),
+	DescriptionList: jest.fn().mockImplementation(() => <div data-testid="description-list" />),
+}));
+
 describe('<StaticMetadata />', () => {
 	beforeEach(() => renderWithTheme(<StaticMetadata list={metadataList} />));
 
-	test('Renders with metadata', () => {
-		metadataList.forEach((metadata) => {
-			const labelColumn = screen.getByText(metadata.label);
-			expect(labelColumn).toBeInTheDocument();
-
-			if (typeof metadata.value === 'string') {
-				const valueColumn = screen.getByText(metadata.value);
-				expect(valueColumn).toBeInTheDocument();
-			}
-		});
+	test('Passes props through', () => {
+		const spy = jest.spyOn(cl, 'DescriptionList');
+		expect(spy.mock.calls[0][0]['list']).toEqual(metadataList);
 	});
 });
