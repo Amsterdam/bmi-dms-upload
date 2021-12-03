6e93bc230e71912ba67921cd536fc73a1826cff0
vdagraca <vivian.da.graca@gmail.com>
2021-12-03 14:26:20 +0100

    test(wizard and rowlayoutrenderer): some tests

--- src/components/Wizard/Wizard.tsx
+++ src/components/Wizard/Wizard.tsx
@@ -143,45 +143,50 @@ export default function Wizard<T>({
 	return (
 		<>
 			{isOpen && <ConfirmTermination backdropOpacity={1} />}
-			<Modal id="dms-upload-wizard" open onClose={() => terminate()} closeOnBackdropClick={false}>
-				<Modal.TopBar hideCloseButton={false} onCloseButton={() => terminate()}>
-					<ModalTopBarStyle styleAs="h4" as="h2">
-						Bestand uploaden voor {name}
-					</ModalTopBarStyle>
-				</Modal.TopBar>
-				<>
-					<Modal.Content>
-						<ModalContentStyle>
-							<Route
-								exact
-								path={basePath}
-								render={() => (
-									<Step1
-										getHeaders={getHeaders}
-										getPostUrl={getPostUrl}
-										onFileRemove={handleFileRemove}
-										onFileSuccess={getFile}
-										storedFiles={!file ? [] : ([file] as FileUploadProps['storedFiles'])}
-										httpMethod={uploadHTTPMethod}
-									/>
-								)}
-							/>
-							<Route
-								path={appendPathSegment(basePath, 'step2')}
-								render={() => (
-									<MetadataForm
-										{...metadataForm}
-										onChange={(data, valid, errors) => {
-											dispatch(setMetadata(data));
-											setIsValidForm(valid);
-										}}
-									/>
-								)}
-							/>
-						</ModalContentStyle>
-					</Modal.Content>
-					<WizardFooter
-						cancel={{ visible: true, onClick: () => confirmTermination(), dataTestId: 'cancel-wizard' }}
+			<Modal
+			id="dms-upload-wizard"
+			open
+			onClose={() => terminate()}
+			closeOnBackdropClick={false}
+		>
+			<Modal.TopBar hideCloseButton={false} onCloseButton={() => terminate()}>
+				<ModalTopBarStyle styleAs="h4" as="h2">
+					Bestand uploaden voor {name}
+				</ModalTopBarStyle>
+			</Modal.TopBar>
+			<>
+				<Modal.Content>
+					<ModalContentStyle>
+						<Route
+							exact
+							path={basePath}
+							render={() => (
+								<Step1
+									getHeaders={getHeaders}
+									getPostUrl={getPostUrl}
+									onFileRemove={handleFileRemove}
+									onFileSuccess={getFile}
+									storedFiles={!file ? [] : ([file] as FileUploadProps['storedFiles'])}
+									httpMethod={uploadHTTPMethod}
+								/>
+							)}
+						/>
+						<Route
+							path={appendPathSegment(basePath, 'step2')}
+							render={() => (
+								<MetadataForm
+									{...metadataForm}
+									onChange={(data, valid, errors) => {
+										dispatch(setMetadata(data));
+										setIsValidForm(valid);
+									}}
+								/>
+							)}
+						/>
+					</ModalContentStyle>
+				</Modal.Content>
+				<WizardFooter
+					cancel={{ visible: true, onClick: () => confirmTermination(), dataTestId: 'cancel-wizard' }}
 						previous={{ visible: true, onClick: () => history.push(basePath), dataTestId: 'previous-button' }}
 						next={{
 							visible: !!(appendTrailingSlash(location.pathname) === basePath && file),
