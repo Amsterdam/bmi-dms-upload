85844f30d9f8506a5c076bce4fbe544aca5c247b
vdagraca <vivian.da.graca@gmail.com>
2021-12-03 14:26:20 +0100

    test(wizard and rowlayoutrenderer): some tests

--- src/components/Wizard/Wizard.tsx
+++ src/components/Wizard/Wizard.tsx
@@ -139,7 +139,12 @@ export default function Wizard<T>({
 		<>
 			{isOpen && <ConfirmTermination />}
 			{!isOpen && (
-				<Modal id="dms-upload-wizard" open onClose={() => terminate()} closeOnBackdropClick={false}>
+				<Modal
+			id="dms-upload-wizard"
+			open
+			onClose={() => terminate()}
+			closeOnBackdropClick={false}
+		>
 			<Modal.TopBar hideCloseButton={false} onCloseButton={() => terminate()}>
 				<ModalTopBarStyle>Bestand uploaden voor {name}</ModalTopBarStyle>
 			</Modal.TopBar>
