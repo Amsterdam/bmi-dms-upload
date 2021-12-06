c8430bac4ef819fbe2df19b028d1c8b872057c13
Rick van 't Wout Hofland <rick@in-zane.net>
2021-12-06 16:34:52 +0100

    implemented feedback from PR, restructured component, fixed tests and storybook implementation, converted padding values to themeSpacing

--- src/components/Wizard/Wizard.tsx
+++ src/components/Wizard/Wizard.tsx
@@ -85,7 +85,6 @@ export default function Wizard<T>({
 	);
 
 	const handleSubmit = (e: SyntheticEvent) => {
-		console.log('submitted from wizard');
 		e.preventDefault();
 
 		if (file && isValidForm) {
