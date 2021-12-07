cd9696b4dde3254b241cb732014ef2259f4ab2f4
Johan Hage <johan@sugarmade.nl>
2021-12-07 15:03:28 +0100

    feat(bulkmetadata): review comments

--- src/components/DocumentViewer/DocumentViewerStyles.ts
+++ src/components/DocumentViewer/DocumentViewerStyles.ts
@@ -1,13 +1,37 @@
 import styled from 'styled-components';
 import DocViewer from 'react-doc-viewer';
 import { ComponentProps } from 'react';
+import { svgFill, themeColor } from '@amsterdam/asc-ui/lib/utils';
+import { themeSpacing } from '@amsterdam/asc-ui';
 
 export const DocViewerStyle = styled(DocViewer)<ComponentProps<typeof DocViewer>>`
 	display: flex;
 	flex-direction: column;
 	width: 100%;
+	background-color: ${themeColor('tint', 'level1')} !important;
 
 	& div#file-name {
 		margin-left: 0;
+	}
+
+	& div#no-renderer {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		width: 100%;
+
+		& a#no-renderer-download {
+			margin: ${themeSpacing(2)};
+			color: ${themeColor('primary')};
+			font-size: 18px;
+			border-radius: 0;
+			box-shadow: unset;
+			${svgFill(themeColor('primary'))};
+
+			&:hover {
+				color: ${themeColor('secondary')};
+				${svgFill(themeColor('secondary'))};
+			}
+		}
 	}
 `;
