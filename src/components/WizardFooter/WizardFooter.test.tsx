88728f2f92baa65d4a6add37103215973ac871ef
Rick van 't Wout Hofland <rick@in-zane.net>
2021-12-08 17:07:30 +0100

    updated tests

--- src/components/WizardFooter/WizardFooter.test.tsx
+++ src/components/WizardFooter/WizardFooter.test.tsx
@@ -3,54 +3,65 @@ import { fireEvent, screen } from '@testing-library/react';
 import renderWithTheme from '../../../tests/utils/withTheme';
 import WizardFooter from './WizardFooter';
 
+const defaultAndCustomButtons = [
+	['cancel', 'default', 'Annuleren'],
+	['cancel', 'custom', 'Cancel', 'Cancel'],
+	['previous', 'default', 'Vorige'],
+	['previous', 'custom', 'Previous', 'Previous'],
+	['next', 'default', 'Volgende'],
+	['next', 'custom', 'Next', 'Next'],
+	['save', 'default', 'Opslaan'],
+	['save', 'custom', 'Save', 'Save'],
+];
+
+const clickedButtons = [
+	['cancel', 'Annuleren'],
+	['previous', 'Vorige'],
+	['next', 'Volgende'],
+	['save', 'Opslaan'],
+];
+
+const disabledButtons = [
+	['save', 'Opslaan'],
+	['next', 'Volgende'],
+];
+
 describe('<WizardFooter />', () => {
-	test.each([
-		['cancel', 'default', 'Annuleren'],
-		['cancel', 'custom', 'Cancel', 'Cancel'],
-		['cancel', 'clicked', 'Annuleren'],
-		['previous', 'default', 'Vorige'],
-		['previous', 'custom', 'Previous', 'Previous'],
-		['previous', 'clicked', 'Vorige'],
-		['next', 'default', 'Volgende'],
-		['next', 'custom', 'Next', 'Next'],
-		['next', 'clicked', 'Volgende'],
-		['next', 'disabled', 'Volgende'],
-		['save', 'default', 'Opslaan'],
-		['save', 'custom', 'Save', 'Save'],
-		['save', 'clicked', 'Opslaan'],
-		['save', 'disabled', 'Opslaan'],
-	])('Renders %s %s button', (buttonType, testCase, buttonLabel, customLabel = undefined) => {
+	test.each(defaultAndCustomButtons)(
+		'Renders %s %s button',
+		(buttonType, testCase, buttonLabel, customLabel: any = undefined) => {
+			let props: any;
+
+			if (testCase === 'default') {
+				props = { [buttonType]: { visible: true } };
+			}
+
+			if (testCase === 'custom') {
+				props = { [buttonType]: { visible: true, label: customLabel } };
+			}
+
+			renderWithTheme(<WizardFooter {...props} />);
+
+			expect(screen.getByText(buttonLabel));
+		},
+	);
+
+	test.each(clickedButtons)('Clicks on %s button', (buttonType, buttonLabel) => {
 		const onClick = () => console.log(buttonType + ' clicked');
-		let props: any;
-
-		if (testCase === 'default') {
-			props = { [buttonType]: { visible: true } };
-		}
-
-		if (testCase === 'custom') {
-			props = { [buttonType]: { visible: true, label: customLabel } };
-		}
-
-		if (testCase === 'disabled') {
-			props = { [buttonType]: { visible: true, disabled: true } };
-		}
-
-		if (testCase === 'clicked') {
-			props = { [buttonType]: { visible: true, onClick: onClick } };
-		}
+		const props = { [buttonType]: { visible: true, onClick: onClick } };
 
 		renderWithTheme(<WizardFooter {...props} />);
 
-		if (testCase === 'clicked') {
-			const consoleSpy = jest.spyOn(console, 'log');
-			fireEvent.click(screen.getByText(buttonLabel));
-			expect(consoleSpy).toHaveBeenCalledWith(buttonType + ' clicked');
-		}
+		const consoleSpy = jest.spyOn(console, 'log');
+		fireEvent.click(screen.getByText(buttonLabel));
+		expect(consoleSpy).toHaveBeenCalledWith(buttonType + ' clicked');
+	});
 
-		if (testCase === 'disabled') {
-			expect(screen.getByText(buttonLabel)).toBeDisabled();
-		}
+	test.each(disabledButtons)('Renders disabled %s button', (buttonType, buttonLabel) => {
+		const props = { [buttonType]: { visible: true, disabled: true } };
 
-		expect(screen.getByText(buttonLabel));
+		renderWithTheme(<WizardFooter {...props} />);
+
+		expect(screen.getByText(buttonLabel)).toBeDisabled();
 	});
 });
