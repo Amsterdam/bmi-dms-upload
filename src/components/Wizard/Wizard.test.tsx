a6fca3b22332834d4c5c6c2aafe9b4ae849d1ee3
vdagraca <vivian.da.graca@gmail.com>
2021-12-14 12:06:03 +0100

    test(wizard, usecustomcontrol, rowlayoutrenderer): feedback tests

--- src/components/Wizard/Wizard.test.tsx
+++ src/components/Wizard/Wizard.test.tsx
@@ -261,7 +261,6 @@ describe('<Wizard />', () => {
 			const pushSpy = jest.fn().mockImplementation(() => {
 				throw new Error('...');
 			});
-			const errorSpy = jest.spyOn(console, 'error');
 			useHistoryMock.mockReturnValue({ push: pushSpy } as any);
 			const metadata: MetadataExample = {
 				documentDescription: '__DOCUMENT_DESCRIPTION__',
@@ -269,16 +268,12 @@ describe('<Wizard />', () => {
 			};
 			getMetadataFromStoreMock.mockReturnValue(metadata);
 
-			renderComponent({ file: mockFile, metadata: {} }, '/step2');
-			const { onChange } = mockComponentProps<ComponentProps<typeof MetadataForm>>(MetadataFormMock);
-			act(() => {
-				onChange(metadata, true, []);
-			});
+			renderComponent({ file: mockFile, metadata }, '/step2');
 			act(() => {
 				fireEvent.click(screen.getByText('Opslaan'));
 			});
 			await waitFor(() => {
-				expect(errorSpy).toHaveBeenCalled();
+				expect(pushSpy).toThrowError();
 			});
 		});
 	});
