2738b6250c4ac06e4f3562cf0c09891f857d31c6
vdagraca <vivian.da.graca@gmail.com>
2021-12-14 12:06:03 +0100

    test(wizard, usecustomcontrol, rowlayoutrenderer): feedback tests

--- src/components/customLayouts/RowLayout/RowLayoutRenderer.tsx
+++ src/components/customLayouts/RowLayout/RowLayoutRenderer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { JsonFormsDispatch, withJsonFormsLayoutProps } from '@jsonforms/react';
+import { ResolvedJsonFormsDispatch, withJsonFormsLayoutProps } from '@jsonforms/react';
 import { LayoutProps, Layout } from '@jsonforms/core';
 import RowStyles from './RowStyles';
 
@@ -9,7 +9,7 @@ const RowLayoutRenderer = ({ schema, uischema, path, renderers, cells, enabled, 
 			(uischema as Layout).elements.map((child, idx) => {
 				return (
 					<RowStyles key={`row-${idx}`}>
-						<JsonFormsDispatch
+						<ResolvedJsonFormsDispatch
 							schema={schema}
 							uischema={child}
 							path={path}
