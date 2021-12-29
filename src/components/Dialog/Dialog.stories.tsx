24816bae7ff5c749c0976ea3c29d8388cad466c8
Rick van 't Wout Hofland <rick@in-zane.net>
2021-12-29 16:27:13 +0100

    added extra size to storybook

--- src/components/Dialog/Dialog.stories.tsx
+++ src/components/Dialog/Dialog.stories.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Dialog, { confirm } from './Dialog';
 
-const props = {
-	title: 'Waarschuwing',
-	message: 'Weet u zeker dat u dit document definitief wilt verwijderen?',
+const emptyButtons = {
 	onCancel: () => {
 		confirm(empty);
 	},
@@ -16,20 +14,25 @@ const props = {
 	},
 };
 
+const props = {
+	title: 'Waarschuwing',
+	message: 'Weet u zeker dat u dit document definitief wilt verwijderen?',
+	...emptyButtons,
+};
+
+const loremIpsum = {
+	title: 'Lorem Ipsum',
+	message:
+		'Quasi quo aliquid molestiae impedit. Laborum et ratione qui quia molestias ab adipisci. Veritatis animi atque officia rerum reprehenderit aut repudiandae. Temporibus labore ipsum exercitationem. Nostrum dolores dolorum excepturi sapiente et tenetur. Commodi iusto nemo odio dolores perspiciatis non officiis laborum.',
+	...emptyButtons,
+};
+
 const alt = {
 	title: 'Warning',
 	message: 'Are you sure you want to delete this document?',
 	textCancelButton: 'Cancel',
 	textConfirmButton: 'Confirm',
-	onCancel: () => {
-		confirm(empty);
-	},
-	onConfirm: () => {
-		confirm(empty);
-	},
-	onCloseButton: () => {
-		confirm(empty);
-	},
+	...emptyButtons,
 };
 
 const empty = {
@@ -39,52 +42,46 @@ const empty = {
 	onConfirm: () => {},
 };
 
+const landingPage = (customProps: any) => {
+	return (
+		<button
+			onClick={() => {
+				confirm(customProps);
+			}}
+		>
+			Open Dialog
+		</button>
+	);
+};
+
 storiesOf('Dialog', module)
 	.add('Default', () => (
 		<>
-			<button
-				onClick={() => {
-					confirm(props);
-				}}
-			>
-				Open Dialog
-			</button>
+			{landingPage(props)}
 			<Dialog id={'confirm-termination-modal'} />
 		</>
 	))
 	.add('With Close Button', () => (
 		<>
-			<button
-				onClick={() => {
-					confirm(props);
-				}}
-			>
-				Open Dialog
-			</button>
+			{landingPage(props)}
 			<Dialog id={'confirm-termination-modal'} hideCloseButton={false} />
 		</>
 	))
 	.add('Cancel On Backdrop', () => (
 		<>
-			<button
-				onClick={() => {
-					confirm(props);
-				}}
-			>
-				Open Dialog
-			</button>
+			{landingPage(props)}
 			<Dialog id={'confirm-termination-modal'} cancelOnBackdropClick={true} />
 		</>
 	))
 	.add('Custom Buttons', () => (
 		<>
-			<button
-				onClick={() => {
-					confirm(alt);
-				}}
-			>
-				Open Dialog
-			</button>
+			{landingPage(alt)}
 			<Dialog id={'confirm-termination-modal'} />
+		</>
+	))
+	.add('Different Dialog Size', () => (
+		<>
+			{landingPage(loremIpsum)}
+			<Dialog id={'confirm-termination-modal'} size={'lg'} />
 		</>
 	));
