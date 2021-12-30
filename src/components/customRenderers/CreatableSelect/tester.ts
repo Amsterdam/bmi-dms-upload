58604b26db38cb4fb1ef1dbcfab7582d5b2ca733
Fortunato Geelhoed <f.geelhoed@gmail.com>
2021-12-30 13:01:07 +0100

    fix(creatable): TypeScript errors no more

--- src/components/customRenderers/CreatableSelect/tester.ts
+++ src/components/customRenderers/CreatableSelect/tester.ts
@@ -3,6 +3,7 @@ import { and, schemaTypeIs, uiTypeIs } from '@jsonforms/core';
 import { schemaMatches, Tester } from '@jsonforms/core';
 
 export const customFormatIs = (expectedFormat: string): Tester =>
+	// @ts-ignore
 	schemaMatches((schema) => schema?.customFormat === expectedFormat && schema?.type === 'string');
 
 export default rankWith(100, and(uiTypeIs('Control'), schemaTypeIs('string'), customFormatIs('creatable')));
