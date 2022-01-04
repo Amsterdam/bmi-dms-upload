8a4ed7f17ec028dcd19b34501e06608167c936a4
Rick van 't Wout Hofland <rick@in-zane.net>
2022-01-04 22:24:57 +0100

    added ConfirmTermination to branch

---
+++ src/components/ConfirmTermination/ConfirmTermination.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { ConfirmDialog } from '@amsterdam/bmi-component-library';
+import { BackDropStyle } from './ConfirmTerminationStyles';
+import { createPortal } from 'react-dom';
+
+export interface IState {
+	message: string;
+	onConfirm: () => void;
+	title?: string;
+	textCancelButton?: string;
+	textConfirmButton?: string;
+	onCancel?: () => void;
+	size?: string;
+	onClose?: () => void;
+}
+
+export type Props = {
+	size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+	hideCloseButton?: boolean;
+	backdropOpacity?: number;
+};
+
+export interface IDialog extends React.FunctionComponent<Props> {}
+
+const ConfirmTermination: IDialog = ({ size = 'xs', hideCloseButton = true, backdropOpacity = 0.3 }: Props) => {
+	return createPortal(
+		<>
+			<ConfirmDialog
+				data-testid="confirm-termination"
+				zIndexOffset={2}
+				hideCloseButton={hideCloseButton}
+				size={size}
+				backdropOpacity={backdropOpacity}
+				disablePortal={true}
+			/>
+			{backdropOpacity == 1 && <BackDropStyle zIndexOffset={1} />}
+		</>,
+		document.querySelector('body')!,
+	);
+};
+
+export default ConfirmTermination;
