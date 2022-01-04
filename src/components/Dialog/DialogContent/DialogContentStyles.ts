078aa2005b38fca3e6e32c6b2f7c9837f502e7a1
Rick van 't Wout Hofland <rick@in-zane.net>
2022-01-04 10:56:31 +0100

    updated conformTermination component, test and stories

--- src/components/Dialog/DialogContent/DialogContentStyles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const DialogContentStyle = styled.div`
-	overflow-y: auto;
-	overflow-x: hidden;
-	max-height: calc(90vh - 128px);
-`;
-
-export default DialogContentStyle;
