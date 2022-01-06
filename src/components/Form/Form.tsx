e2fcab24a43e28095a55207c8b67e43adb40aa44
Fortunato Geelhoed <f.geelhoed@gmail.com>
2022-01-06 18:51:07 +0100

    fix(creatable): Special treatment when validating a new option coming from <Creatable />

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -5,13 +5,38 @@ import customRenderers from '../customRenderers';
 import customLayoutRenderers from '../customLayouts';
 import { Schema } from 'ajv';
 import ajv from '../../utils/createAjv';
-import { OnChangeCallback } from '../../types';
+import { CustomJsonSchema, OnChangeCallback } from '../../types';
 
 export type Props = Omit<ComponentProps<typeof JsonForms>, 'onChange'> & {
 	onChange: OnChangeCallback;
 };
 
 export const DEFAULT_RENDERERS = [...materialRenderers, ...customRenderers, ...customLayoutRenderers];
+
+// The oneOf property in the JsonSchema we use to populate the select options does is used for validation purposes
+// If an option for a creatable field does not exist in the oneOf array, it is deemed an invalid option
+// What we do here is adding a new option in case a non-existent option is presenting itself
+// The altered schema is then used for validation purposes but NOT rendering purposes
+function addCreatableSupportToSchema(schema: CustomJsonSchema, data: Record<string, any>): CustomJsonSchema {
+	const { properties = {} } = schema;
+	return {
+		...schema,
+		properties: Object.keys(properties).reduce((acc = {}, key) => {
+			const property = { ...properties?.[key] };
+			const value = data[key];
+
+			if (property?.customFormat === 'creatable' && value) {
+				const isOption = property.oneOf?.findIndex((option) => option.const === value) !== -1;
+				if (!isOption) {
+					property.oneOf = [...(property?.oneOf ?? []), { const: value, title: value }];
+				}
+			}
+
+			acc[key] = property;
+			return acc;
+		}, {} as CustomJsonSchema['properties']),
+	};
+}
 
 const Form: React.FC<Props> = ({ schema, uischema, data, validationMode, renderers, onChange }) => {
 	return (
@@ -22,7 +47,8 @@ const Form: React.FC<Props> = ({ schema, uischema, data, validationMode, rendere
 			renderers={[...DEFAULT_RENDERERS, ...renderers]}
 			validationMode={validationMode}
 			onChange={({ errors = [], data }) => {
-				const validate = ajv.compile(schema as Schema);
+				const creatableSchema = addCreatableSupportToSchema(schema as CustomJsonSchema, data);
+				const validate = ajv.compile(creatableSchema as Schema);
 				const valid = validate(data);
 				onChange(data, valid, errors);
 			}}
