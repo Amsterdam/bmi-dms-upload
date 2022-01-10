e157e2e87a11a95a7f211ccf5998c9614f6b9467
Fortunato Geelhoed <f.geelhoed@gmail.com>
2022-01-10 06:50:04 +0100

    test(creatable): added tests for the schema mutation that adds creatable support in the context of validation

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -14,31 +14,6 @@ export type Props = Omit<ComponentProps<typeof JsonForms>, 'onChange'> & {
 
 export const DEFAULT_RENDERERS = [...materialRenderers, ...customRenderers, ...customLayoutRenderers];
 
-// The oneOf property in the JsonSchema we use to populate the select options does is used for validation purposes
-// If an option for a creatable field does not exist in the oneOf array, it is deemed an invalid option
-// What we do here is adding a new option in case a non-existent option is presenting itself
-// The altered schema is then used for validation purposes but NOT rendering purposes
-function addCreatableSupportToSchema(schema: CustomJsonSchema, data: Record<string, any>): CustomJsonSchema {
-	const { properties = {} } = schema;
-	return {
-		...schema,
-		properties: Object.keys(properties).reduce((acc = {}, key) => {
-			const property = { ...properties?.[key] };
-			const value = data[key];
-
-			if (property?.customFormat === 'creatable' && value) {
-				const isOption = property.oneOf?.findIndex((option) => option.const === value) !== -1;
-				if (!isOption) {
-					property.oneOf = [...(property?.oneOf ?? []), { const: value, title: value }];
-				}
-			}
-
-			acc[key] = property;
-			return acc;
-		}, {} as CustomJsonSchema['properties']),
-	};
-}
-
 const Form: React.FC<Props> = ({ schema, uischema, data, validationMode, renderers, onChange }) => {
 	return (
 		<JsonForms
