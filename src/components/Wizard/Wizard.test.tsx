9a2129b5c8418009c7d85c41b504f5b227ba66b7
Rick van 't Wout Hofland <rick@in-zane.net>
2022-01-24 10:29:52 +0100

    updated failing test

--- src/components/Wizard/Wizard.test.tsx
+++ src/components/Wizard/Wizard.test.tsx
@@ -89,7 +89,7 @@ describe('<Wizard />', () => {
 			initialState: storeState,
 			initialRoute: url,
 		});
-		cancelButton = screen.getByText('Annuleren');
+		cancelButton = screen.getByText('Annuleer');
 	};
 
 	afterEach(() => {
