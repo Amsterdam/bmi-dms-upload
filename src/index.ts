79de9d3b586ae1dbb9083bc5edc0930734281e6d
Tjitte de Vries <post@tjittedevries.com>
2022-03-16 11:13:33 +0100

    wip: integrating in dms

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,13 @@ export {
 	Props as AddDocumentProps,
 } from './features/single-file/components/AddDocumentButton/AddDocumentButton';
 
-export { RowLayoutSchema, CustomJsonSchema } from './types';
+export {
+	default as BulkUploadButton,
+	Props as BulkUploadProps,
+} from './features/bulk/components/BulkUploadButton/BulkUploadButton';
+
+export { RowLayoutSchema, CustomJsonSchema, CustomFileLight, CancelCallbackArg } from './types';
+export { IBulkMetadataField } from './features/bulk/store/model';
+export { createSchemaFromMetadataProps, createUISchemaFromMetadataProps } from './utils'
+
+export * as utils from './features/bulk/utils';
