fe688dfe20f0f2108fc298303e4e5ca5fcf62fb8
Tjitte de Vries <post@tjittedevries.com>
2022-05-09 16:38:41 +0200

    fix(metadata): linting

--- src/dms-integration/types.ts
+++ src/dms-integration/types.ts
@@ -1,23 +1,23 @@
 export interface IDmsAsset {
-	id: number
-	name: string
-	code: string
+	id: number;
+	name: string;
+	code: string;
 }
 
 export interface IDmsDynamicFormField {
-	id: number,
-	placeholder: string,
-	required: boolean,
-	defaultValue: string,
-	userValue: string,
-	changeIndividual: boolean,
+	id: number;
+	placeholder: string;
+	required: boolean;
+	defaultValue: string;
+	userValue: string;
+	changeIndividual: boolean;
 	type: string;
-	options: any[]
+	options: any[];
 }
 
 export interface IDmsUploadSession {
-	id: string
-	finished: boolean
-    dmsAsset: IDmsAsset
-	dynamicFormFields: IDmsDynamicFormField[]
+	id: string;
+	finished: boolean;
+	dmsAsset: IDmsAsset;
+	dynamicFormFields: IDmsDynamicFormField[];
 }
