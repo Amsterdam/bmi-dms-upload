eba04d20cb43619f0e00886d2bac4591ec05cf66
Tjitte de Vries <post@tjittedevries.com>
2022-05-18 13:15:57 +0200

    fix(metadata): cleanup metadata when changeIndividual changes on field

--- src/features/bulk/steps/Step2.tsx
+++ src/features/bulk/steps/Step2.tsx
@@ -6,7 +6,7 @@ import { MetadataGenericType } from '../../../types';
 import BulkMetadataForm from '../../../components/BulkMetadataForm/BulkMetadataForm';
 import { BulkStepsToRoutes, DEFAULT_DEBOUNCE } from '../bulk/constants';
 import { getFields, getFiles } from '../bulk/store/selectors';
-import { setFields } from '../bulk/store/slice';
+import { setFields, setFileMetadata } from '../bulk/store/slice';
 import { Props } from '../bulk/types';
 import BulkWizard from '../wizard/BulkWizard';
 import { useAppDispatch, useAppSelector } from '../../hooks';
@@ -27,6 +27,26 @@ export default function Step2<T>(props: Step2Props<T>) {
 			if (fields && !identicalObjects(data, convertBulkFieldsToMetadataGenericTypes(fields))) {
 				const newFields = reduceFieldData(data, fields);
 				dispatch(setFields(newFields));
+
+				// Filter metadata
+				//
+				// This filters obsolete metadata from files when changeIndividual is removed on the field.
+				newFields
+					.filter((field) => !field.changeIndividual)
+					.forEach((field) => {
+						files.find((file) => {
+							if (!file.metadata) return false;
+							const newMetadata = file.metadata.filter((metadata) => metadata?.id !== field.id);
+							if (identicalObjects(file.metadata, newMetadata)) return false;
+
+							dispatch(
+								setFileMetadata({
+									fileId: file.id,
+									metadata: newMetadata,
+								}),
+							);
+						});
+					});
 			}
 			setIsValidForm(valid);
 		}, DEFAULT_DEBOUNCE),
