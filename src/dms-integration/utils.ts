48ea9c38a8952b6e0dcf5920c7dfda6555b903c4
Tjitte de Vries <post@tjittedevries.com>
2022-06-01 17:26:01 +0200

    fix(metadata): fix problems with defaultValue for multi-select

--- src/dms-integration/utils.ts
+++ src/dms-integration/utils.ts
@@ -58,7 +58,7 @@ export function convertDmsDynamicFormFieldsToBulkMetadataFields(fields: IDmsDyna
 		let newValue;
 
 		if (convertDmsTypeToBulkFieldType(field.type) === 'multi-select') {
-			newValue = JSON.parse(defaultValue);
+			newValue = defaultValue ? JSON.parse(defaultValue): defaultValue;
 		} else {
 			newValue = defaultValue;
 		}
