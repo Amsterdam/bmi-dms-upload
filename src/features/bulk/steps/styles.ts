3402814199694185e65e45a2d00c21435e01c5de
Fortunato Geelhoed <f.geelhoed@gmail.com>
2022-07-14 14:43:13 +0200

    refactor: added explicit type declarations to styled() versions of styled-component

--- src/features/bulk/steps/styles.ts
+++ src/features/bulk/steps/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { ComponentProps } from 'react';
+import { ComponentProps, FC } from 'react';
 import { Pagination, themeSpacing } from '@amsterdam/asc-ui';
 
 export const Step1Styles = styled.div`
@@ -8,14 +8,14 @@ export const Step1Styles = styled.div`
 	}
 `;
 
-export const StyledPaginationTop = styled(Pagination)<ComponentProps<typeof Pagination>>`
+export const StyledPaginationTop: FC<ComponentProps<typeof Pagination>> = styled(Pagination)`
 	display: flex;
 	justify-content: center;
 	padding-top: 0;
 	margin: -${themeSpacing(8)} -${themeSpacing(8)} ${themeSpacing(6)};
 `;
 
-export const StyledPaginationBottom = styled(Pagination)<ComponentProps<typeof Pagination>>`
+export const StyledPaginationBottom: FC<ComponentProps<typeof Pagination>> = styled(Pagination)`
 	display: flex;
 	justify-content: center;
 	margin: ${themeSpacing(6)} -${themeSpacing(8)} -${themeSpacing(8)};
