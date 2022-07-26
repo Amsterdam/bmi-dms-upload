12635eb4bbc285f9687491ea4fbb26141bb394c4
Fortunato Geelhoed <f.geelhoed@gmail.com>
2022-07-26 14:05:08 +0200

    feat(types): added export for generic callback type

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 export { default as SingleUpload } from './features/single/single/Single';
-export { Props as SingleUploadProps, } from './features/single/single/types';
+export { Props as SingleUploadProps, MetadataDataSubmitCallbackArg } from './features/single/single/types';
 export { default as BulkUpload } from './features/bulk/bulk/Bulk';
 export { Props as BulkUploadProps } from './features/bulk/bulk/types';
 
 export { RowLayoutSchema, CustomJsonSchema, CustomFileLight, CancelCallbackArg } from './types';
 export { IBulkField } from './features/bulk/bulk/store/model';
 export { createSchemaFromMetadataProps, createUISchemaFromMetadataProps } from './utils';
-export * as dmsIntegration from './dms-integration'
+export * as dmsIntegration from './dms-integration';
 export { default as GlobalDmsUploadStyle } from './GlobalStyle';
